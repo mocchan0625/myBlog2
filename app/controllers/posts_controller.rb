8caa0228035464c2978520ca762b434fc4de4406
mocchan0625 <yuya.mottate0625@gmail.com>
2015-01-03 03:46:51 +0900

    コメント機能の追加

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,61 @@
+class PostsController < ApplicationController
+
+  #GET /posts/new
+
+  def new
+    @post = Post.new
+  end
+
+  # POST /posts
+def create
+  @post = Post.new(post_params)
+
+  if @post.save
+    redirect_to @post
+  else
+    render 'new'
+  end
+end
+
+   # GET /posts/:id
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  #GET /posts
+  def index
+    @posts = Post.all
+  end
+
+  #GET /posts/:id/edit
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  # PATCH /posts/:id
+  # PUT   /posts/:id
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+    # DELETE /posts/:id
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
+end
